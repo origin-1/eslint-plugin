74d9e1a47554d8fdefec6c0e21dd186caa14ddc2
Francesco Trotta <github@fasttime.org>
2022-11-26 16:47:32 +0100

    New rule `property-colon-spacing`

--- readme.md
+++ readme.md
@@ -31,7 +31,8 @@ the `"rules"` section.
     ],
     "rules": {
         "@origin-1/nice-space-before-function-paren": "error",
-        "@origin-1/no-spaces-in-call-expression": "error"
+        "@origin-1/no-spaces-in-call-expression": "error",
+        "@origin-1/property-colon-spacing": "error"
     }
 }
 ```
@@ -200,6 +201,90 @@ fn // this is fine, too
 #### Further Reading
 
 * [ESLint Issue #7587](https://github.com/eslint/eslint/issues/7587)
+
+### `property-colon-spacing`
+
+#### Rule Details
+
+The rule `property-colon-spacing` enforces consistent spacing around the colon of key-value
+properties in object literals.
+
+The rule requires that a property has no spaces before the colon if it is pre-adjustable, and that
+is has a consistent, nonzero number of spaces after the colon if it is post-adjustable.
+
+A property is considered pre-adjustable if the colon and the first token or comment before the colon
+share a line of code, and it is post-adjustable if the colon and the first token or comment after
+the colon share a line of code.
+
+If a property is post-adjustable, the rule requires exactly one space after the colon if one of the
+following criteria is true:
+* The property shares a line of code with another property in the same object.
+* The first token or comment after the colon does not start on the same line of code where the
+  property starts.
+* It is the only post-adjustable property in the object that does not meet the two criteria above.
+
+Other post-adjustable properties that do not meet these criteria will be aligned by the rule such
+that the first tokens or comments after the colons start at the same column.
+
+Only key-value properties are validated.
+Shortcut properties, spread properties, accessor and method definitions are not validated.
+
+Examples of **incorrect** code for this rule:
+
+```js
+/* eslint @origin-1/property-colon-spacing: "error" */
+/* eslint-env es2018 */
+
+var obj1 = { foo : bar };
+
+var obj2 = { foo:bar };
+
+var obj3 = { foo:  bar };
+
+method
+(
+    {
+        foo: bar,
+        bar:  foo,
+    },
+);
+
+```
+
+Examples of **correct** code for this rule:
+
+```js
+/* eslint @origin-1/property-colon-spacing: "error" */
+/* eslint-env es2018 */
+
+var obj1 = { foo: 1, [bar]: 2, 'baz': 3 };
+
+var obj2 =
+{
+    foo: 1,
+    shortcut,
+};
+
+var obj3 =
+{
+    foo:     1,
+     [bar]:  2,
+    'baz':   3,
+    [
+        'multiline key'
+    ]: 4,
+    'value on next line':
+        5,
+    shortcut,
+    get accessor()
+    { },
+    set accessor(value)
+    { },
+    method()
+    { },
+    ...spread,
+};
+```
 
 [npm badge]: https://badge.fury.io/js/@origin-1%2Feslint-plugin.svg
 [npm url]: https://www.npmjs.com/package/@origin-1/eslint-plugin
---
+++ test/lib/rules/property-colon-spacing.js
@@ -0,0 +1,297 @@
+'use strict';
+
+const rule             = require('../../../lib/rules/property-colon-spacing');
+const { RuleTester }   = require('eslint');
+
+const ruleTester = new RuleTester();
+const tests =
+{
+    valid:
+    [
+        {
+            code:
+            '({ foo: bar, [bar]: foo, 1: 1, baz, a() { }, get b() { }, set b(b) { }, ...c })',
+            parserOptions: { ecmaVersion: 2018 },
+        },
+        {
+            code:
+            `
+            ({ a
+                 :
+                   b })
+            `,
+        },
+        {
+            code: '({ a /* - */: b })',
+        },
+        {
+            code:
+            `
+            ({
+                a:      'A',
+                bb:     'B',
+                ccc:    'C',
+            })
+            `,
+        },
+        {
+            code:
+            `
+            foo = {
+                "🌷": "bar", // 2 code points
+                "🎁": "baz", // 2 code points
+                "🇮🇳": "qux", // 4 code points
+                "🏳️‍🌈": "xyz", // 6 code points
+            };
+            `,
+        },
+        {
+            code:
+            `
+            const obj =
+            {
+                foo:     1,
+                 [bar]:  2,
+                'baz':   3,
+                [
+                    'multiline key'
+                ]: 4,
+                'value on next line':
+                    5,
+                shortcut,
+                get accessor()
+                { },
+                set accessor(value)
+                { },
+                method()
+                { },
+                ...spread,
+            };
+            `,
+            parserOptions: { ecmaVersion: 2018 },
+        },
+    ],
+    invalid:
+    [
+        {
+            code:
+            '({ a() { }, get b() { }, set b(b) { }, ...c, foo : bar })',
+            output:
+            '({ a() { }, get b() { }, set b(b) { }, ...c, foo: bar })',
+            parserOptions: { ecmaVersion: 2018 },
+            errors:
+            [
+                {
+                    messageId:  'extraSpaceBeforeColon',
+                    data:       { computed: '', key: 'foo' },
+                    line:       1,
+                    column:     49,
+                    endLine:    1,
+                    endColumn:  50,
+                    type:       'Punctuator',
+                },
+            ],
+        },
+        {
+            code:
+            '({ __proto__ : null, foo : bar, [bar] : foo, "baz" : baz, 1 : 42, [a+\nb] : c })',
+            output:
+            '({ __proto__: null, foo: bar, [bar]: foo, "baz": baz, 1: 42, [a+\nb]: c })',
+            parserOptions: { ecmaVersion: 2018 },
+            errors:
+            [
+                { messageId: 'extraSpaceBeforeColon', data: { computed: '', key: '__proto__' } },
+                { messageId: 'extraSpaceBeforeColon', data: { computed: '', key: 'foo' } },
+                { messageId: 'extraSpaceBeforeColon', data: { computed: 'computed ', key: 'bar' } },
+                { messageId: 'extraSpaceBeforeColon', data: { computed: '', key: 'baz' } },
+                { messageId: 'extraSpaceBeforeColon', data: { computed: '', key: '1' } },
+                {
+                    messageId:  'extraSpaceBeforeColon',
+                    data:       { computed: 'computed ', key: 'a+\nb' },
+                },
+            ],
+        },
+        {
+            code:
+            `
+            ({ a
+            /* - */ : b })
+            `,
+            output:
+            `
+            ({ a
+            /* - */: b })
+            `,
+            errors:
+            [
+                {
+                    messageId:  'extraSpaceBeforeColon',
+                    line:       3,
+                    column:     20,
+                    endLine:    3,
+                    endColumn:  21,
+                },
+            ],
+        },
+        {
+            code:   '({a\t\f\v:\xa0\u2000b})',
+            output: '({a:\xa0b})',
+            errors:
+            [
+                {
+                    messageId:  'extraSpaceBeforeColon',
+                    line:       1,
+                    column:     4,
+                    endLine:    1,
+                    endColumn:  7,
+                },
+                {
+                    messageId:  'extraSpaceAfterColon',
+                    line:       1,
+                    column:     8,
+                    endLine:    1,
+                    endColumn:  10,
+                },
+            ],
+        },
+        {
+            code:   'foo = { bar:baz }',
+            output: 'foo = { bar: baz }',
+            errors:
+            [
+                {
+                    messageId:  'missingSpaceAfterColon',
+                    data:       { computed: '', key: 'bar' },
+                    line:       1,
+                    column:     13,
+                    endLine:    1,
+                    endColumn:  13,
+                    type:       'Punctuator',
+                },
+            ],
+        },
+        {
+            code:
+            `
+            ({
+                foo: 1,
+                bar:        2,
+                ...baz,
+                foobar:3,
+                barfoo:  4,
+                baz:
+                5,
+            })
+            `,
+            output:
+            `
+            ({
+                foo:     1,
+                bar:     2,
+                ...baz,
+                foobar:  3,
+                barfoo:  4,
+                baz:
+                5,
+            })
+            `,
+            parserOptions: { ecmaVersion: 2018 },
+            errors:
+            [
+                {
+                    messageId:  'missingSpaceAfterColon',
+                    data:       { computed: '', key: 'foo' },
+                    line:       3,
+                    column:     21,
+                    endLine:    3,
+                    endColumn:  22,
+                },
+                {
+                    messageId:  'extraSpaceAfterColon',
+                    data:       { computed: '', key: 'bar' },
+                    line:       4,
+                    column:     21,
+                    endLine:    4,
+                    endColumn:  29,
+                },
+                {
+                    messageId:  'missingSpaceAfterColon',
+                    data:       { computed: '', key: 'foobar' },
+                    line:       6,
+                    column:     24,
+                    endLine:    6,
+                    endColumn:  24,
+                },
+            ],
+        },
+        {
+            code:
+            `
+            var x =
+            {
+                foobar:1,
+                baz: 2,
+            };
+            `,
+            output:
+            `
+            var x =
+            {
+                foobar: 1,
+                baz:    2,
+            };
+            `,
+            errors:
+            [
+                { messageId: 'missingSpaceAfterColon', data: { computed: '', key: 'foobar' } },
+                { messageId: 'missingSpaceAfterColon', data: { computed: '', key: 'baz' } },
+            ],
+        },
+        {
+            code:
+            '({ __proto__:  null, foo:  bar, [bar]:  foo, "baz":  baz, 1:  42, [a+\nb]:  c })',
+            output:
+            '({ __proto__: null, foo: bar, [bar]: foo, "baz": baz, 1: 42, [a+\nb]: c })',
+            parserOptions: { ecmaVersion: 2015 },
+            errors:
+            [
+                { messageId: 'extraSpaceAfterColon', data: { computed: '', key: '__proto__' } },
+                { messageId: 'extraSpaceAfterColon', data: { computed: '', key: 'foo' } },
+                { messageId: 'extraSpaceAfterColon', data: { computed: 'computed ', key: 'bar' } },
+                { messageId: 'extraSpaceAfterColon', data: { computed: '', key: 'baz' } },
+                { messageId: 'extraSpaceAfterColon', data: { computed: '', key: '1' } },
+                {
+                    messageId:  'extraSpaceAfterColon',
+                    data:       { computed: 'computed ', key: 'a+\nb' },
+                },
+            ],
+        },
+        {
+            code:
+            `
+            (
+                {
+                    foo:  bar,
+                    bar : foo,
+                }
+            )
+            `,
+            output:
+            `
+            (
+                {
+                    foo:  bar,
+                    bar:  foo,
+                }
+            )
+            `,
+            errors:
+            [
+                { messageId: 'extraSpaceBeforeColon' },
+                { messageId: 'missingSpaceAfterColon' },
+            ],
+        },
+    ],
+};
+
+ruleTester.run('property-colon-spacing', rule, tests);
